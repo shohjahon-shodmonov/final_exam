113f941b0e3108b507df70dfb6e6c60419ee86b8
Shohjahon <sh.shodmonov1@gmail.com>
2022-05-16 15:08:02 +0500

    size config

---
+++ lib/core/extensions/mediaquery_exte.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+extension ForSize on BuildContext {
+  MediaQueryData get mq => MediaQuery.of(this);
+
+  double get h => mq.size.height;
+  double get w => mq.size.width;
+}
